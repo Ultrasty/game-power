2d3bf47a9bed92e2b2b75342bcbc55e36b833360
DESKTOP-COJJGQV\win10 <761447951@qq.com>
2021-05-06 17:25:27 +0800

    frogEnemy

---
+++ Assets/Katana/Script/Enemy/Enemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    public int health;
+    public int damage;
+    public float flashTime;
+    public GameObject bloodEffect;//流血特效
+
+    private SpriteRenderer sr;
+    private Color originalColor;
+    //private PlayerHealth playerHealth;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        //获取player对象下的playerHealth组件
+        //playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+        sr = GetComponent<SpriteRenderer>();
+        originalColor = sr.color;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        FlashColor(flashTime);
+        Instantiate(bloodEffect, transform.position, Quaternion.identity);
+        //GameController.camShake.Shake();
+    }
+
+    //受伤红光闪烁时间time
+    void FlashColor(float time)
+    {
+        sr.color = Color.red;
+        Invoke("ResetColor", time);//延迟调用重置颜色
+    }
+
+    void ResetColor()
+    {
+        sr.color = originalColor;
+    }
+    /*
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //检测是否碰撞到玩家
+        if (other.gameObject.CompareTag("Player") && other.GetType().ToString()== "UnityEngine.CapsuleCollider2D")
+        {
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damage);
+            }
+        }
+    }*/
+}
