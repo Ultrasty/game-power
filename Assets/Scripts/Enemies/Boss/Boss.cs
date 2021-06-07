5e28749946f7817e7b041ff47c32aab9e7078078
shinenine <1205748010@qq.com>
2021-06-07 20:31:42 +0800

    boss

---
+++ Assets/Scripts/Enemies/Boss/Boss.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+
+	public Transform player;
+
+	public bool isFlipped = false;
+
+	public void LookAtPlayer()
+	{
+		Vector3 flipped = transform.localScale;
+		flipped.z *= -1f;
+
+		if (transform.position.x > player.position.x && isFlipped)
+		{
+			Debug.Log("1");
+			transform.localScale = flipped;
+			transform.Rotate(0f, 180f, 0f);
+			isFlipped = false;
+		}
+		else if (transform.position.x < player.position.x && !isFlipped)
+		{
+			Debug.Log("2");
+			transform.localScale = flipped;
+			transform.Rotate(0f, 180f, 0f);
+			isFlipped = true;
+		}
+	}
+
+}
