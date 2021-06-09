b83403d43c7160187aaba35f37052cceab0ab04b
shinenine <1205748010@qq.com>
2021-06-09 17:02:52 +0800

    fix conflict

---
+++ Assets/Scripts/Enviroment/Traps/sawDamage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sawDamage : MonoBehaviour
+{
+    public int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+
+        // if (collider.IsTouching(playerFeetCollider))
+        // {
+            
+        //     HealthBar player = playerFeetCollider.GetComponent<HealthBar>();
+        //     player.TakeDamage(damage);
+        // }
+
+    }
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D hitInfo)
+    {
+        HealthBar player = hitInfo.gameObject.GetComponent<HealthBar>();
+        
+        if (player != null)
+        {
+            player.TakeDamage(damage);
+        }
+    }
+}
