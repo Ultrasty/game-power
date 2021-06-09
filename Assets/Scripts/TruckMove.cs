c87dfbf4706de867f703d4ea6bbe0f1df415d289
needle1989 <edwardskirk@163.com>
2021-06-09 20:38:27 +0800

    finish scene1

---
+++ Assets/Scripts/TruckMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckMove : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Interact()
+    {
+        anim.SetBool("isTruck", true);
+    }
+
+}
