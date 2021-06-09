a2c7f8223c1b9ae9d25626cf2ede4a1a3dbbc333
gjh <761447951@qq.com>
2021-06-09 18:41:34 +0800

    Walker

---
+++ Assets/Scripts/Enemies/Walker/WalkerAttack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkerAttack : MonoBehaviour
+{
+    [SerializeField] private LayerMask layer;
+    private WalkerController2 walker;
+    private PolygonCollider2D col;
+    private GameObject player;
+
+    private void Start()
+    {
+        walker = GameObject.Find("Flyer").GetComponent<WalkerController2>();
+        col = GetComponent<PolygonCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.transform.name == "Player")
+        {
+            // get player's health bar script and TakeDamage()
+            player = GameObject.Find("Player");
+            player.BroadcastMessage("TakeDamage", walker.attackDamage);
+        }
+    }
+}
