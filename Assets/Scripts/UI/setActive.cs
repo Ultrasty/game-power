1490a2fb7f917132d464899c6a74151eb1411066
shinenine <1205748010@qq.com>
2021-06-11 11:18:05 +0800

    完善了场景2.1并且增加了过关条件脚本

---
+++ Assets/Scripts/UI/setActive.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setActive : MonoBehaviour
+{
+  public List<GameObject> objects;
+  public GameObject portal;
+  public GameObject collectMask;
+  private bool ifComplete;
+  // Start is called before the first frame update
+  void Start()
+  {
+    ifComplete = false;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    for (int i = 0; i < objects.Count; i++)
+    {
+      if (objects[i])
+      {
+        ifComplete = false;
+        break;
+      }
+      else
+      {
+        ifComplete = true;
+      }
+    }
+    if (ifComplete)
+    {
+      portal.gameObject.SetActive(true);
+      collectMask.gameObject.SetActive(false);
+    }
+  }
+}
