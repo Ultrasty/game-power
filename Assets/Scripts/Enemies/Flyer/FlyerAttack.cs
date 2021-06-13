923c01dee3140677cbd58517bb1eb35db3f1d973
shinenine <1205748010@qq.com>
2021-06-13 10:55:39 +0800

    将scene1和scene2.0的过关条件统一改为setActive

--- Assets/Scripts/Enemies/Flyer/FlyerAttack.cs
+++ Assets/Scripts/Enemies/Flyer/FlyerAttack.cs
@@ -4,44 +4,45 @@ using UnityEngine;
 
 public class FlyerAttack : MonoBehaviour
 {
-    [SerializeField] public float attackDamage = 1.0f;
-    public float time;
-    public float startTime;
-    private Animator anim;
-    private PolygonCollider2D col;
-    private GameObject player;
+  [SerializeField] public float attackDamage = 1.0f;
+  public float time;
+  public float startTime;
+  private Animator anim;
+  private PolygonCollider2D col;
+  private GameObject player;
 
-    private void Start()
-    {
-        anim = GameObject.Find("Flyer").GetComponent<Animator>();
-        col = GetComponent<PolygonCollider2D>();
-    }
-    private void FixedUpdate()
-    {
-    }
+  private void Start()
+  {
+    anim = GameObject.Find("Flyer").GetComponent<Animator>();
+    col = GetComponent<PolygonCollider2D>();
+  }
+  private void FixedUpdate()
+  {
+  }
 
-    public void Attack()
-    {
-        anim.SetBool("Attack", true);
-        StartCoroutine(disableHitBox());
-    }
-    IEnumerator startAttack()
-    {
-        yield return new WaitForSeconds(startTime);
-        col.enabled = true;
-        StartCoroutine(disableHitBox());
-    }
-    IEnumerator disableHitBox()
-    {
-        yield return new WaitForSeconds(time);
-        col.enabled = false;
-    }
+  public void Attack()
+  {
+    anim.SetBool("Attack", true);
+    StartCoroutine(disableHitBox());
+  }
+  IEnumerator startAttack()
+  {
+    yield return new WaitForSeconds(startTime);
+    col.enabled = true;
+    StartCoroutine(disableHitBox());
+  }
+  IEnumerator disableHitBox()
+  {
+    yield return new WaitForSeconds(time);
+    col.enabled = false;
+  }
 
-    void OnTriggerEnter2D(Collider2D other)
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    Debug.Log("213");
+    if (other.gameObject.CompareTag("Player"))
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            other.GetComponent<HealthBar>().TakeDamage(attackDamage);
-        }
+      other.GetComponent<HealthBar>().TakeDamage(attackDamage);
     }
+  }
 }
