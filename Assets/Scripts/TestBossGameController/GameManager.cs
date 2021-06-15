e9473c97327c47d43c93bcc2a414aa5c10aa6ee8
sty <1455670697@qq.com>
2021-06-15 21:42:34 +0800

    准备添加字幕的转场

---
+++ Assets/Scripts/TestBossGameController/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] Boss; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Boss.Length == 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
