2d0ea78681a6c02513f3edaa2490807ba43b4ea4
shinenine <1205748010@qq.com>
2021-06-15 23:00:31 +0800

    finish

--- Assets/Scripts/TestBossGameController/GameController.cs
+++ Assets/Scripts/TestBossGameController/GameController.cs
@@ -4,37 +4,22 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
 
-    void OnGUI()
-    {
+  // Update is called once per frame
+  void Update()
+  {
 
-        GUI.Box(new Rect(10, 10, 100, 90), "Debug Menu");
+  }
 
-        if (GUI.Button(new Rect(20, 40, 80, 20), "win"))
-        {
-            success();
-        }
 
-        if (GUI.Button(new Rect(20, 70, 80, 20), "lose"))
-        {
-            
-        }
-    }
+  public void success()
+  {
 
-    public void success()
-    {
-
-    }
+  }
 
 }
